a392577f5b0834ca111e693243acedae8419347d
Alexandre <areny1998@hotmail.com>
2019-04-17 16:29:49 -0400

    wtf

---
+++ Projet1/moto.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include "VehiculePromenade.h"
+using namespace std;
+
+class Moto : public VehiculePromenade
+{
+protected:
+
+public:
+	Moto();
+	Moto(const Moto&);
+	virtual ~Moto();
+	 bool setImmatriculation(string);
+	 int getVitesse() ;
+	string getSymbole() ;
+};
---
+++ Projet1/pompier.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <string>
+#include "vehiculeUrgence.h"
+using namespace std;
+
+class Pompier : public VehiculeUrgence
+{
+protected:
+
+public:
+	Pompier();
+	Pompier(const Pompier&);
+	virtual ~Pompier();
+	virtual int getVitesse();
+	virtual string getSymbole();
+	virtual bool setImmatriculation(string);
+};
